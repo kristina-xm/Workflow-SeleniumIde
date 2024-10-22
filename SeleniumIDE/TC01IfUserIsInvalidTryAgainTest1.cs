f248b1052a2f1acc8cd783ad7b50d1d7be63e4ef
kristina-xm <kristina.hmn@gmail.com>
2024-10-22 13:15:16 +0300

    Change driverPath

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -27,7 +27,8 @@ public class TC01IfUserIsInvalidTryAgainTest
         options.AddArguments("disable-dev-shm-usage");
         options.AddArguments("disable-gpu");
         options.AddArguments("window-size=1920x1080");
-        string driverPath = @"C:\Users\krist\desktop\2-seleniumide\drivers";
+
+        string driverPath = Path.Combine(Directory.GetCurrentDirectory(), "drivers");
 
         var service = ChromeDriverService.CreateDefaultService(driverPath);
         driver = new ChromeDriver(service, options); // Initialize driver
